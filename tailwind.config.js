6089402348c2ca40b794e65a183593bd09c94674
Efunnuga Bamidele <bamideleefunnuga9@gmail.com>
2022-11-17 09:21:18 +0100

    feat: Create Custom CSS Classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
   theme: {
     extend: {
       colors:{
-        primary: '#FF6363'
+        primary: '#FF6363',
+        secondary: {
+          100: "#E2E2D5",
+          200: "#888883"
+
+        }
+      },
+      fontFamily: {
+        body: ['Nunito']
       }
     },
   },
